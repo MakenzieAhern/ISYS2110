f54adb5b8f606afb27e704fe14c4e4eaebe6ef27
MakenzieAhern <167406552+MakenzieAhern@users.noreply.github.com>
2025-05-24 20:03:28 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from routes import register_routes
+import os
 
 app = Flask(__name__)
 app.secret_key = "key"
 
 register_routes(app)
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
